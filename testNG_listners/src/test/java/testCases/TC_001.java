01c1bd1a524c952566b223387d5fb5420037667f
Lakshan Sithara <124455976+Lakshan-sithara@users.noreply.github.com>
2025-12-01 18:48:50 +0530

    Add test to count total links on page
    Introduced tc_002 test method to count and print the total number of <a> elements on the page using Selenium WebDriver.

--- testNG_listners/src/test/java/testCases/TC_001.java
+++ testNG_listners/src/test/java/testCases/TC_001.java
@@ -2,12 +2,21 @@ package testCases;
 
 import base.initializeBrowser;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class TC_001 extends initializeBrowser {
 
     @Test
     public void tc_001(){
         driver.findElement(By.xpath("//button[@id='submit']")).click();
+    }
+
+    @Test
+    public void tc_002(){
+        List<WebElement> link = driver.findElements(By.xpath("//a"));
+        System.out.println("Total number of links are: " + link.size());
     }
 }
